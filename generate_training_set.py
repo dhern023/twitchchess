fcc4f1ce826c70af4d18c970e0da02fe762e4eb7
dhern023 <dhern023@ucr.edu>
2025-05-18 20:26:56 -0400

    Abstract extraction function

--- generate_training_set.py
+++ generate_training_set.py
@@ -5,6 +5,19 @@ import pathlib
 from state import State
 
 def get_dataset(num_samples=None):
+def extract_boards(instance_game):
+    """
+    Extracts serialized board from game
+    """
+    board = instance_game.board()
+    list_out = []
+    for move in instance_game.mainline_moves():
+        board.push(move)
+        board_serialized = State(board).serialize()
+        list_out.append(board_serialized)
+
+    return list_out
+
   X,Y = [], []
   gn = 0
   dict_map_result = {'1/2-1/2':0, '0-1':-1, '1-0':1}
@@ -18,14 +31,15 @@ def get_dataset(num_samples=None):
       result = game.headers['Result']
       if result not in dict_map_result:
         continue
-      board = game.board()
-      for i, move in enumerate(game.mainline_moves()):
-        board.push(move)
-        ser = State(board).serialize()
-        X.append(ser)
-        Y.append(value)
       value = dict_map_result[result]
+
+      # extract
+      list_boards_serialized = extract_boards(game)
+      X.extend(list_boards_serialized)
+      Y.extend([value] * len(list_data))
       print("parsing game %d, got %d examples" % (gn, len(X)))
+
+      # break early
       if num_samples is not None and len(X) > num_samples:
         return X,Y
       gn += 1
